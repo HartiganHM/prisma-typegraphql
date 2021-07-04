e9fa05a7fa567134836ae90efd77e9a3e25776e3
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-04 11:37:19 -0600

    Fix Post schema

--- src/post/Post.ts
+++ src/post/Post.ts
@@ -1,29 +1,29 @@
-import { ObjectType, Field } from "type-graphql";
+import { ObjectType, Field, ID } from "type-graphql";
 
 import { User } from "user";
 
 @ObjectType()
 class Post {
-  @Field()
+  @Field(() => ID)
   id: number;
 
-  @Field()
+  @Field(() => Date)
   createdAt: Date;
 
-  @Field()
+  @Field(() => Date)
   updatedAt: Date
 
   @Field()
   title: string;
 
-  @Field()
+  @Field(() => String, { nullable: true })
   content: string | null;
 
   @Field()
   published: boolean;
 
-  // @Field()
-  // author: User;
+  @Field(() => User, { nullable: true })
+  author?: User | null
 
   @Field()
   authorId: number;
