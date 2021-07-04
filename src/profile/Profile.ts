c51d78bf38d8e667eab00620242762cd43026c7f
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-07-04 12:03:38 -0600

    Build initial Profile schema

---
+++ src/profile/Profile.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field, ID } from "type-graphql";
+import { User } from "user";
+
+@ObjectType()
+class Profile {
+  @Field(() => ID)
+  id: number;
+
+  @Field()
+  bio?: string;
+
+  @Field(() => User, { nullable: true })
+  user: User;
+
+  @Field()
+  userId: number;
+}
+
+export default Profile;
